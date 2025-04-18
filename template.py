c7ffe9a18b765ae7edfffb8d4b97cfabadafda21
Abhishek Kori <abhishekkori601@gmail.com>
2025-04-18 16:06:01 +0530

    folder added

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(leve=logging.info, format="[%(asctime)s]: %(message)s:")
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
 
 project_name = "Text Summarization"
 
@@ -43,7 +43,6 @@ for filepath in list_of_files:
             logging.info(f"Creating empty file: {filepath}")
 
 
-    
     else:
         logging.info(f"{filename} is already exists")
 
